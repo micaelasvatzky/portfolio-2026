8cff8d8aab74231427889c4dc7651572df60b222
micaelasvatzky <svatzkymicaela@gmail.com>
2026-01-12 21:33:42 -0300

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='mt-10 h-8 bg-[#B9471C] flex justify-center items-center'>
-        <p className='text-[#EFE6DA]'>Micaela Svatzky - 2026</p>
+    <div className="fixed bottom-0 w-full h-8 bg-[#B9471C] flex justify-center items-center">
+      <p className="text-[#EFE6DA]">Micaela Svatzky - 2026</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
